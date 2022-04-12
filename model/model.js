f3002ab6f7183db7850e177bca6bd90af9b8e273
JalenHew <99741776+JalenHew@users.noreply.github.com>
2022-04-11 21:49:33 -0400

    origin

---
+++ model/model.js
@@ -0,0 +1,357 @@
+var homeContent = `<div class="banner">
+<div class="title">Travel-Fly</div>
+<div class="text">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis esse iusto sapiente quod sint vel, eligendi
+    exercitationem labore repellat consequuntur sed quidem vitae nesciunt sit dignissimos aliquid omnis eveniet
+    laboriosam?
+</div>
+<div class="readMore">READ MORE</div>
+</div>
+
+<div class="pictures">
+<div class="tour-one">
+    <div class="nation">ISRAEL</div>
+    <div class="price">FROM $1000</div>
+    <div class="learnMore">LEARN MORE</div>
+</div>
+<div class="tour-two">
+    <div class="nation">U.S.A</div>
+    <div class="price">FROM $1500</div>
+    <div class="learnMore">LEARN MORE</div>
+</div>
+<div class="tour-three">
+    <div class="nation">AUSTRALIA</div>
+    <div class="price">FROM $1800</div>
+    <div class="learnMore">LEARN MORE</div>
+</div>
+</div>
+<div class="form-content">
+<h1>Booking form</h1>
+<div class="nameholder">
+    <div class="nameMail">
+        <div class="holder">
+            <input type="text" id="fname" placeholder="name">
+            <!-- <i class="icon far fa-calendar"></i> -->
+        </div>
+
+        <div class="holder">
+            <input class="long-box" type="email" id="femail" placeholder="email">
+            <!-- <i class="icon far fa-calendar"></i> -->
+        </div>
+
+    </div>
+    <div class="nameMail">
+        <div class="holder">
+            <input type="text" id="fname" placeholder="country">
+            <!-- <i class="icon far fa-calendar"></i> -->
+        </div>
+
+        <div class="holder">
+            <input class="long-box" type="text" id="femail" placeholder="hotel">
+            <!-- <i class="icon far fa-calendar"></i> -->
+        </div>
+    </div>
+</div>
+<div class="checkIn">
+    <div class="ctext">Check-in</div>
+    <div class="holder">
+        <input class="longerBox" type="date" placeholder="MM/DD/YYYY">
+        <!-- <i class="icon far fa-calendar"></i> -->
+    </div>
+</div>
+<div class="checkIn">
+    <div class="ctext">Check-out</div>
+    <div class="holder">
+        <input class="longerBox" type="date" placeholder="MM/DD/YYYY">
+        <!-- <i class="icon far fa-calendar"></i> -->
+
+    </div>
+</div>
+<div class="tresHermanos">
+    <div class="container">
+        <div class="holder">
+            <div class="adult">Adult</div>
+            <input class="short-box" type="number" placeholder="2">
+            <div class="down-one">
+                <i class="fas fa-caret-down"></i>
+            </div>
+        </div>
+
+        <div class="holder-holder">
+            <div class="holder">
+                <div class="adult">Children</div>
+                <input class="short-box" type="number" placeholder="0">
+                <div class="down-two">
+                    <i class="fas fa-caret-down"></i>
+                </div>
+            </div>
+        </div>
+        <div class="holder">
+            <div class="adult">Rooms</div>
+            <input class="short-box" type="number" placeholder="1">
+            <div class="down-three">
+                <i class="fas fa-caret-down"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="message">
+    <div class="holder">
+        <input class="big-box" type="text" placeholder="message...">
+    </div>
+</div>
+<div class="submit">SUBMIT</div>
+<div></div>
+</div>`;
+var aboutContent = `<div class="fullpage">
+            <p class="aboutText">ABOUT:</p>
+            <div class="line"></div>
+            <div class="about-container">
+
+
+                <div class="about-text">
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae
+                        dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                        fugit,
+                        sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
+                        quisquam
+                        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+                        eius
+                        modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+                </div>
+            </div>
+            <div class="client-container">
+                <p></p>
+                <div class="miranda-brown">
+                    <p>CLIENT QUOTES:</p>
+                    <div class="line"></div>
+                    <div class="client-one">
+                        <div class="client-one-img"></div>
+                        <div class="client-one-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                            veritatis et quasi architecto beatae vitae dicta sunt explicabo. </div>
+                        <div class="client-name">- Miranda Brown</div>
+
+                    </div>
+
+                </div>
+                <div class="john-wes">
+                    <div class="client-two">
+                        <div class="client-two-img"></div>
+                        <div class="client-two-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                            veritatis et quasi architecto beatae vitae dicta sunt explicabo. </div>
+                        <div class="client-name">- Johnathan Wes</div>
+
+                    </div>
+                </div>
+            </div>
+        </div>`;
+var toursContent = ` <div class="content">
+<header>
+    <div class="headerText">TOURS:</div>
+</header>
+<div class="tours">
+    <div class="israel">
+        <div class="countryName">Israel</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="usa">
+        <div class="countryName">U.S.A</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="australia">
+        <div class="countryName">Australia</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="newZealand">
+        <div class="countryName">New Zealand</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="france">
+        <div class="countryName">Israel</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="egypt">
+        <div class="countryName">Israel</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+</div>
+<div class="tours">
+    <div class="japan">
+        <div class="countryName">Israel</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="canada">
+        <div class="countryName">Israel</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+    <div class="uae">
+        <div class="countryName">U.A.E</div>
+        <div class="price">from $1000</div>
+        <div class="learnMore">
+            <p class="learnText">LEARN MORE</p>
+        </div>
+    </div>
+</div>
+</div>`;
+var speacialOffersContent = `<div class="fullpage">
+<!--START DYNAMIC CONTENT-->
+<div class="container">
+    <div class="specialOffers">
+        <div class="specialHeader">
+            <p>SPECIAL OFFER(S):</p>
+            <div class="UnderLine"></div>
+        </div>
+        <div class="Barcelona">
+            <div class="BarcelonaImg"></div>
+            <div class="BarcelonaTxt">
+                <div class="BarcelonaHeader">
+                    <p>BARCELONA, SPAIN</p>
+                    <p class="twentyPercent">(20% Off!)</p>
+                </div>
+                <div class="UnderLine"></div>
+                <div class="BarcelonaAbout">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+                    veritatis et quasi architecto beatae vitae dicta sunt explicabo. </div>
+                <div class="LearnMore">LEARN MORE</div>
+            </div>
+        </div>
+
+        <div class="Bangkok">
+            <div class="BangkokImg"></div>
+            <div class="BangkokTxt">
+                <div class="BangkokHeader">
+                    <p>BANGKOK, THAILAND</p>
+                    <p class="twentyPercent">(20% Off!)</p>
+                </div>
+                <div class="UnderLine"></div>
+                <div class="BangkokAbout">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                    accusantium
+                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                    quasi
+                    architecto beatae vitae dicta sunt explicabo. </div>
+                <div class="LearnMore">LEARN MORE</div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!--END-->
+<div class="footer">
+    <div class="logo"></div>
+    <div class="copyright">Copyright 2019 travel-fly. All rights reserved</div>
+    <div class="media">
+        <i class="fab fa-facebook-square"></i>
+        <i class="fab fa-instagram-square"></i>
+        <i class="fab fa-twitter-square"></i>
+    </div>
+</div>
+</div>`;
+var blogContent = ` <div class="container">
+<div class="header">
+    <div class="blog">BLOG:</div>
+    <div class="UnderLine"></div>
+</div>
+<div class="blogHome">
+    <div class="date">
+        <p class="dateNum">06</p>
+        <p class="month">JUN</p>
+    </div>
+    <div class="picText">
+        <div class="blogPic"></div>
+        <div class="blogText">
+            <p class="bigText">Sed et persipiatis unde omnis iste natus</p>
+            <p class="blueText">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                quasi
+                architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+    </div>
+</div>
+<div class="blogHome_two">
+    <div class="date_two">
+        <p class="dateNum">23</p>
+        <p class="month">JUL</p>
+    </div>
+    <div class="picText_two">
+        <div class="blogPic_two"></div>
+        <div class="blogText_two">
+            <p class="bigText">Sed et persipiatis unde omnis iste natus</p>
+            <p class="blueText">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+                doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et
+                quasi
+                architecto beatae vitae dicta sunt explicabo. </p>
+        </div>
+    </div>
+
+</div>`;
+var contactContent = ` <div class="fullpage">
+<div class="container">
+    <div class="header">
+        <div class="blog">CONTACT US:</div>
+        <div class="UnderLine"></div>
+    </div>
+    <div class="conContainer">
+
+        <div class="mapAddress">
+            <div class="map"></div>
+            <div class="address">
+                travel-fly Inc.<wbr>
+            8901 Marmora Road,<wbr>
+            Glasgow, D04 89GR.<wbr>
+            Freephone:+1 800 559 6580<wbr>
+            Telephone:+1 800 603 6035<wbr>
+            FAX:+1 800 889 9898<wbr>
+            E-mail: mail@travelfly.org<wbr>
+            </div>
+        </div>
+        <div class="formSend">
+            <div class="form">
+                <input type="text" class="box" placeholder="Your name...">
+                <input type="text" class="box" placeholder="Email Address...">
+                <input type="text" class="box" placeholder="Company...">
+                <input type="text" class="fatBox" placeholder="Message...">
+            </div>
+            <div class="send">
+                <p>SEND MESSAGE</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`;
+
+
+
+export function modelPageName(pgName) {
+    console.log(pgName);
+    
+    $("#app").html(eval(pgName));
+}
